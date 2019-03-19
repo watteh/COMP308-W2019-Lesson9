0cb1b321a0f82a129210fbba434d0a22825b4c53
Ryan <ryan.bg.watson@gmail.com>
2019-03-18 22:06:10 -0400

    added user logout - closes #6

--- client/src/app/partials/header/header.component.ts
+++ client/src/app/partials/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private flashMessage: FlashMessagesService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.user = new User();
+    this.user = JSON.parse(localStorage.getItem('user'));
+  }
+
+  onLogoutClick(): void {
+    this.authService.logout().subscribe(data => {
+      this.flashMessage.show(data.msg, {cssClass: 'alert-warning', timeOut: 5000});
+      this.router.navigate(['/login']);
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.loggedIn();
   }
 
 }
